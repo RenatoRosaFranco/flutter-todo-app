ca4c294c537dc72eaf91e0119f230399238c4fa4
Renato Rosa Franco <renato_ny@live.com>
2022-10-22 10:46:16 -0300

    Project initialized and navbar created

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:news_app/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent
+      )
+    );
+
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo App',
+      home: Home(),
+    );
+  }
+}
