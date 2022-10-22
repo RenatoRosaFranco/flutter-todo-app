bac85b719e106e162db7f1b2d81b175c10b6e4ec
Renato Rosa Franco <renato_ny@live.com>
2022-10-22 11:07:55 -0300

    Search bar added to home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,29 +8,87 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
-        appBar: AppBar(
-            backgroundColor: tdBGColor,
-            elevation: 0,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Icon(
-                  Icons.menu,
-                  color: tdBlack,
-                  size: 30
-                ),
-                SizedBox(
-                  height: 40,
-                  width: 40,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.asset('assets/images/avatar.jpeg'),
-                  ),
-                )
-              ],
-            )
+        appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(60),
+          child: buildAppBar(),
         ),
-        body: const Text('This is home screen')
+        body: Container(
+          padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 15
+          ),
+          child: Column(
+            children: [
+              searchBox()
+            ],
+          ),
+        )
+    );
+  }
+}
+
+// Widgets
+class buildAppBar extends StatelessWidget {
+  const buildAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30
+            ),
+            SizedBox(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/avatar.jpeg'),
+              ),
+            )
+          ],
+        )
+    );
+  }
+}
+class searchBox extends StatelessWidget {
+  const searchBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20)
+      ),
+      child: const TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(
+            color: tdGrey
+          )
+        ),
+      ),
     );
   }
 }
